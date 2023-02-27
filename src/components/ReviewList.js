2f640321fc752a1c26418f0c1b1362c7cfa17e8a
Lee-na-yun <happyin3333@nate.com>
2023-02-27 15:39:54 +0900

    각 item의 title을 list로 가져오기

--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -1,6 +1,8 @@
 function ReviewList({items}) { // items라는 배열을 prop으로 받음 -> items에 있는 각 요소를 리액트로 렌더링할 것임!
-  console.log(items);
-  return <ul></ul>;
+  return <ul>{items.map((item)=>{
+        return <li>{item.title}</li>
+    })}
+    </ul>;
 }
 
 export default ReviewList;
