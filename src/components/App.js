77af66a3835c1443603d32095328371e34175e0d
Lee-na-yun <happyin3333@nate.com>
2023-02-27 16:53:22 +0900

    order state로 최신순으로 정렬하기

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import ReviewList from "./ReviewList";
 import items from "../mock.json"; // items라는 이름으로 mock.json파일을 import함
+import { useState } from "react";
 
-function App() { // 프로젝트의 최상위 컴포넌트
-    const sortedItems = items.sort((a,b)=>b.rating - a.rating); //내림차순
+function App() {
+  // 프로젝트의 최상위 컴포넌트
+  const [order, setOrder] = useState("createdAt");
+  const sortedItems = items.sort((a, b) => b[order] - a[order]); //order State값이 createdAt에 있을 때는 최신순으로 정렬되고, rating일 때는 평점이 높은 베스트순으로 정렬이 됨
 
-    return <div><ReviewList items={sortedItems}/></div>
+  return (
+    <div>
+      <ReviewList items={sortedItems} />
+    </div>
+  );
 }
 
 export default App;
