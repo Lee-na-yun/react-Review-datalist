feeda1638500c6374de002bc3d3cb239e8dd4b22
Lee-na-yun <happyin3333@nate.com>
2023-02-27 16:48:29 +0900

    rating 리뷰 높은것부터 내림차순으로 sort를 사용하여 배열 재정렬하기

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,9 @@ import ReviewList from "./ReviewList";
 import items from "../mock.json"; // items라는 이름으로 mock.json파일을 import함
 
 function App() { // 프로젝트의 최상위 컴포넌트
-    return <div><ReviewList items={items}/></div>
+    const sortedItems = items.sort((a,b)=>b.rating - a.rating); //내림차순
+
+    return <div><ReviewList items={sortedItems}/></div>
 }
 
 export default App;
