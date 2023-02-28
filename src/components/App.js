16521f06d330b29bee2ec7cd5e60aae2835b6bb4
Lee-na-yun <happyin3333@nate.com>
2023-02-28 11:07:51 +0900

    filter 메서드로 삭제하기

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,25 @@
 import ReviewList from "./ReviewList";
-import items from "../mock.json"; // items라는 이름으로 mock.json파일을 import함
+import mockItems from "../mock.json"; // items라는 이름으로 mock.json파일을 import함
 import { useState } from "react";
 
 function App() {
   // 프로젝트의 최상위 컴포넌트
+  const [items, setItems] = useState(mockItems);
   const [order, setOrder] = useState("createdAt");
   const sortedItems = items.sort((a, b) => b[order] - a[order]); //order State값이 createdAt에 있을 때는 최신순으로 정렬되고, rating일 때는 평점이 높은 베스트순으로 정렬이 됨
 
-  const handleNewClick = () => setOrder('createdAt');
-  const handleBestClick = () => setOrder('rating');
+  const handleNewClick = () => setOrder("createdAt");
+  const handleBestClick = () => setOrder("rating");
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id); // id를 바탕으로 filter 함수로 걸러낸 배열이 새로운 State값으로 변경됨
+    setItems(nextItems);
+  };
 
   return (
     <div>
-        <button onClick={handleNewClick}>최신순</button>
-        <button onClick={handleBestClick}>추천순</button>
-      <ReviewList items={sortedItems} />
+      <button onClick={handleNewClick}>최신순</button>
+      <button onClick={handleBestClick}>추천순</button>
+      <ReviewList items={sortedItems} onDelete={handleDelete}/>
     </div>
   );
 }
--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -5,7 +5,9 @@ function formatDate(value) {
   return `${date.getFullYear()}, ${date.getMonth() + 1}, ${date.getDate()}`;
 }
 
-function ReviewListItem({ item }) {
+function ReviewListItem({ item, onDelete }) {
+  const handleDeleteClick = () => onDelete(item.id); // onDelete 함수를 item의 id값으로 실행함
+
   return (
     <div className="ReviewListItem">
       <img className="ReviewListItem_img" src={item.imgUrl} alt={item.title} />
@@ -15,19 +17,20 @@ function ReviewListItem({ item }) {
         <p>{formatDate(item.createdAt)}</p>{" "}
         {/* createdAt이 숫자형이라 값을 formatDate함수로 만들어줌*/}
         <p>{item.content}</p>
+        <button onClick={handleDeleteClick}>삭제</button>
       </div>
     </div>
   );
 }
 
-function ReviewList({ items }) {
+function ReviewList({ items, onDelete }) {
   // items라는 배열을 prop으로 받음 -> items에 있는 각 요소를 리액트로 렌더링할 것임!
   return (
     <ul>
       {items.map((item) => {
         return (
           <li>
-            <ReviewListItem item={item} />
+            <ReviewListItem item={item} onDelete={onDelete} />
           </li>
         ); //ReviewListItem 컴포넌트 추가
       })}
