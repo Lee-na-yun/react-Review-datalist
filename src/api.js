daec3b74c99b8e9a364fb5e7c332c4d7733a1bf8
Lee-na-yun <happyin3333@nate.com>
2023-03-02 18:09:56 +0900

    페이지네이션으로 데이터 추가로 불러오기 기능

--- src/api.js
+++ src/api.js
@@ -1,11 +1,17 @@
 // request 함수를 모아 놓고 사용하는 파일!
 
 // fetch를 호출하고 받아온 response body를 return하는 함수
-export async function getReviews(order = 'createdAt') {
-    const query = `order=${order}`;
-    const response = await fetch(`https://learn.codeit.kr/7008/film-reviews?${query}`)
-    const body = await response.json();
-    return body;
-} 
+export async function getReviews({
+  order = "createdAt",
+  offset = 0,
+  limit = 6,
+}) {
+  const query = `order=${order}&offset=${offset}&limit=${limit}`;
+  const response = await fetch(
+    `https://learn.codeit.kr/7008/film-reviews?${query}`
+  );
+  const body = await response.json();
+  return body;
+}
 // async~await : 네트워크 request를 기다렸다가 response에서 json함수를 호출하고 json 변환이 끝나면 body를 return해 줌
 // async를 사용 ==> getReviews 함수 == 비동기 함수
--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,13 @@ import ReviewList from "./ReviewList";
 import { useEffect, useState } from "react";
 import { getReviews } from "../api";
 
+const LIMIT = 6;
+
 function App() {
   // 프로젝트의 최상위 컴포넌트
   const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt");
+  const [offset, setOffset] = useState(0);
   const sortedItems = items.sort((a, b) => b[order] - a[order]); //order State값이 createdAt에 있을 때는 최신순으로 정렬되고, rating일 때는 평점이 높은 베스트순으로 정렬이 됨
 
   const handleNewClick = () => setOrder("createdAt");
@@ -16,13 +19,23 @@ function App() {
     setItems(nextItems);
   };
 
-  const handleLoad = async (orderQuery) => {
-    const { reviews } = await getReviews(orderQuery); // response body에 있는 reviews라는 값을 Destructuring 하고
-    setItems(reviews); // state 변경
+  const handleLoad = async (options) => {
+    const { reviews } = await getReviews(options); // response body에 있는 reviews라는 값을 Destructuring 하고
+    if (options.offset === 0) {
+      setItems(reviews); // offset값이 0일 때 items 전체를 바꾸고
+    } else {
+      setItems([...items, ...reviews]); // offset값이 0이 아닐 때는 spread문법으로 요소가 추가된 배열을 만들어 줌
+    }
+    setOffset(options.offset + reviews.length); // offset값 변경
   };
 
+  // 다음페이지를 불러올 함수
+  const handleLoadMore = () => {
+    handleLoad({ order, offset, limit: LIMIT });
+  }
+
   useEffect(() => {
-    handleLoad(order);
+    handleLoad({ order, offset: 0, limit: LIMIT });
   }, [order]);
 
   return (
@@ -30,6 +43,7 @@ function App() {
       <button onClick={handleNewClick}>최신순</button>
       <button onClick={handleBestClick}>추천순</button>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
+      <button onClick={handleLoadMore}>더 보기</button>
     </div>
   );
 }
