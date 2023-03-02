df514a9eb4894006527628d833300d6af8a54009
Lee-na-yun <happyin3333@nate.com>
2023-03-02 10:44:26 +0900

    fetch 함수 사용하여 버튼 클릭시 네트워크 데이터 불러오기

---
+++ src/api.js
@@ -0,0 +1,10 @@
+// request 함수를 모아 놓고 사용하는 파일!
+
+// fetch를 호출하고 받아온 response body를 return하는 함수
+export async function getReviews() {
+    const response = await fetch('https://learn.codeit.kr/7008/film-reviews/')
+    const body = await response.json();
+    return body;
+} 
+// async~await : 네트워크 request를 기다렸다가 response에서 json함수를 호출하고 json 변환이 끝나면 body를 return해 줌
+// async를 사용 ==> getReviews 함수 == 비동기 함수
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import ReviewList from "./ReviewList";
-import mockItems from "../mock.json"; // items라는 이름으로 mock.json파일을 import함
+//import mockItems from "../mock.json"; // items라는 이름으로 mock.json파일을 import함
 import { useState } from "react";
+import { getReviews } from "../api";
 
 function App() {
   // 프로젝트의 최상위 컴포넌트
-  const [items, setItems] = useState(mockItems);
+  const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt");
   const sortedItems = items.sort((a, b) => b[order] - a[order]); //order State값이 createdAt에 있을 때는 최신순으로 정렬되고, rating일 때는 평점이 높은 베스트순으로 정렬이 됨
 
@@ -15,11 +16,17 @@ function App() {
     setItems(nextItems);
   };
 
+  const handleLoadClick = async () => {
+    const {reviews} = await getReviews(); // response body에 있는 reviews라는 값을 Destructuring 하고
+    setItems(reviews); // state 변경
+  }
+
   return (
     <div>
       <button onClick={handleNewClick}>최신순</button>
       <button onClick={handleBestClick}>추천순</button>
       <ReviewList items={sortedItems} onDelete={handleDelete}/>
+      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 }
